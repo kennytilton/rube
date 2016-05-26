a7434057cc0412c64cf77b2a0364579087be4fc3
Kenneth Tilton <kentilton@gmail.com>
2016-05-26 13:09:42 -0400

    qxia begins

--- project.clj
+++ project.clj
@@ -5,25 +5,22 @@
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.7.0"]
                            [org.clojure/clojurescript "1.7.48"]]
-            :plugins [[lein-cljsbuild "1.0.6"]
-                      [lein-midje "3.1.3"]]
+            :plugins [[lein-cljsbuild "1.1.3"]]
             :test-paths ["test"]
-            :cljsbuild {:builds {:minify {:source-paths ["src"]
-                                          :compiler {:output-to "resources/public/js/main.js"
-                                                     :output-dir "cljsbuild-output-minify"
-                                                     :optimizations :advanced
-                                                     :pretty-print false
-                                                     :externs ["externs/jquery-externs-1.9.js"
-                                                               "externs/externs.js"
-                                                               "externs/moment.js"]}}
-                                 :dev {:source-paths ["src"]
-                                       :compiler {:output-to "resources/public/js/main.js"
-                                                  :output-dir "resources/public/js/build-output-dev"
+            :cljsbuild {:builds {;; go back to 5/26 for minify
+                                 :qxia {:source-paths ["src"]
+                                       :compiler {:output-to "resources/identica/source/script/qxia.js"
+                                                  :output-dir "resources/identica/source/script/out"
                                                   :optimizations :whitespace}}
-                                 :test {:source-paths ["src" "test"]
-                                       :compiler {:output-to "resources/public/js/main-test.js"
-                                                  :optimizations :whitespace
-                                                  :pretty-print true}}}
+                                 ;; :dev {:source-paths ["src"]
+                                 ;;       :compiler {:output-to "resources/public/js/main.js"
+                                 ;;                  :output-dir "resources/public/js/out"
+                                 ;;                  :optimizations :whitespace}}
+                                 ;; :test {:source-paths ["src" "test"]
+                                 ;;       :compiler {:output-to "resources/public/js/main-test.js"
+                                 ;;                  :optimizations :whitespace
+                                 ;;                  :pretty-print true}}
+                                 }
                         :test-commands {"unit" ["phantomjs"
                                                 "resources/test/phantom/runner.js"
                                                 "resources/test/test.html"]}} )
